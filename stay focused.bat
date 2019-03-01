92826681dfa904b74d81d0f3117323bad4a38a89
Shahab <shab.salami@gmail.com>
2019-03-01 17:30:06 +0330

    add stay focused bat script

---
+++ stay focused.bat
@@ -0,0 +1,10 @@
+@echo off
+title Stay focuse
+
+Taskkill   /F /IM slack.exe
+Taskkill   /F /IM chrome.exe
+Taskkill   /F /IM firefox.exe
+Taskkill   /F /IM Telegram.exe
+Taskkill   /F /IM GoogleUpdate.exe
+Taskkill   /F /IM GoogleCrashHandler.exe
+ 
